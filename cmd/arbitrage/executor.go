8b3f66f358f517cc2a32ad1d79f92bdd393ba397
yaonam <elimviolinist@gmail.com>
2023-08-17 21:41:20 -0400

    Added ExitStrategy

--- cmd/arbitrage/executor.go
+++ cmd/arbitrage/executor.go
@@ -16,12 +16,11 @@ import (
 func EnterStrategy(wallet string, ps map[string]*p.Protocol, strategy []*t.MarketInfo, initialCollateralAmount *big.Float, ltvFactor *big.Float) error {
 	currentAmount := initialCollateralAmount
 	for i, market := range strategy {
-		if i%2 == 1 { // Lend
-			p, ok := ps[market.Protocol+":"+market.Chain]
-			if !ok {
-				return fmt.Errorf("protocol chain not found: %s:%s", market.Protocol, market.Chain)
-			}
-			// Lend
+		p, ok := ps[market.Protocol+":"+market.Chain]
+		if !ok {
+			return fmt.Errorf("protocol chain not found: %s:%s", market.Protocol, market.Chain)
+		}
+		if i%2 == 0 { // Lend
 			amount, _ := currentAmount.Int(nil)
 			tx, err := (*p).Supply(wallet, market.Token, amount)
 			if err != nil {
@@ -29,16 +28,11 @@ func EnterStrategy(wallet string, ps map[string]*p.Protocol, strategy []*t.Marke
 			}
 			log.Printf("Lent %v %v to %v (%v)", amount, market.Token, market.Protocol, tx.Hash())
 		} else { // Borrow
-			p, ok := ps[market.Protocol+":"+market.Chain]
-			if !ok {
-				return fmt.Errorf("protocol chain not found: %s:%s", market.Protocol, market.Chain)
-			}
 			// Calculate the amount to lend
 			ltv := new(big.Float).Quo(market.LTV, big.NewFloat(100))
 			ltv.Quo(ltv, ltvFactor)
 			currentAmount.Mul(currentAmount, ltv)
 			amount, _ := currentAmount.Int(nil)
-			// Borrow
 			tx, err := (*p).Borrow(wallet, market.Token, amount)
 			if err != nil {
 				return fmt.Errorf("failed to borrow: %v", err)
@@ -53,6 +47,25 @@ func EnterStrategy(wallet string, ps map[string]*p.Protocol, strategy []*t.Marke
 // Repays slightly more than debt to account for interest.
 func ExitStrategy(wallet string, ps map[string]*p.Protocol, strategy []*t.MarketInfo) error {
 	// Starting with the end, withdraw all collateral and repay all debt
-
+	for i := len(strategy) - 1; i >= 0; i-- {
+		market := strategy[i]
+		p, ok := ps[market.Protocol+":"+market.Chain]
+		if !ok {
+			return fmt.Errorf("protocol chain not found: %s:%s", market.Protocol, market.Chain)
+		}
+		if i%2 == 0 { // Withdraw
+			tx, err := (*p).WithdrawAll(wallet, market.Token)
+			if err != nil {
+				return fmt.Errorf("failed to withdraw: %v", err)
+			}
+			log.Printf("Withdrew all %v from %v (%v)", market.Token, market.Protocol, tx.Hash())
+		} else { // Repay
+			tx, err := (*p).RepayAll(wallet, market.Token)
+			if err != nil {
+				return fmt.Errorf("failed to repay: %v", err)
+			}
+			log.Printf("Repayed all %v to %v (%v)", market.Token, market.Protocol, tx.Hash())
+		}
+	}
 	return nil
 }
